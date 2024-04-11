94a3f64713e31f3488fecdd7d56edfe7e9a690c4
Tian Feng <tian.feng@saucelabs.com>
2024-04-11 14:11:47 -0700

    rewrite using python2

--- misc/download.py
+++ misc/download.py
@@ -6,37 +6,39 @@ import os
 import hashlib
 
 def download_and_unzip(url, zip_file_path, target_folder):
-    # Download the file in chunks
     try:
-        with urlopen(url) as response:
-            with open(zip_file_path, 'wb') as out_file:
-                while True:
-                    chunk = response.read(64 * 1024)
-                    if not chunk:
-                        break
-                    out_file.write(chunk)
+        response = urlopen(url)
+        with open(zip_file_path, 'wb') as out_file:
+            while True:
+                chunk = response.read(64 * 1024)  # 64 KB chunks
+                if not chunk:
+                    break
+                out_file.write(chunk)
         print("Download completed successfully.")
     except Exception as e:
-        print("Error downloading the file:", e)
+        print("Error downloading the file: {}".format(e))
         return
 
+    # Calculate the actual size of the file
     actual_size = os.path.getsize(zip_file_path)
-    print('file size: ', actual_size)
+    print("Downloaded file size: {} bytes".format(actual_size))
 
+    # Calculate MD5 hash of the downloaded file
     hash_md5 = hashlib.md5()
     with open(zip_file_path, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
     md5_hash = hash_md5.hexdigest()
-    print('md5sum: ', md5_hash)
+    print("MD5 hash of the downloaded file: {}".format(md5_hash))
 
+    # Unzip the file using subprocess and unzip command
     try:
         subprocess.check_call(["unzip", "-o", "-q", zip_file_path, "-d", target_folder])
-        print("File successfully extracted")
+        print("File successfully extracted to {}".format(target_folder))
     except subprocess.CalledProcessError as e:
-        print("Failed to extract ZIP file: ", e)
+        print("Failed to extract ZIP file: {}".format(e))
 
-# Example usage
+
 url = "https://storage.googleapis.com/sauce-devx-runners-bhtb/cypress-macos-amd64-8622143467.zip"
 zip_file_path = 'cypress-macos-amd64-8622143467.zip'
 target_folder = './' 
