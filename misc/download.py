477cfee93c7e75e4ca9b982fcf6134f023a13e6c
Tian Feng <tian.feng@saucelabs.com>
2024-04-12 11:51:26 -0700

    use thread to read file

--- misc/download.py
+++ misc/download.py
@@ -4,16 +4,56 @@ from six.moves.urllib.request import urlopen
 import subprocess
 import os
 import hashlib
+import threading
+
+READ_BUFFER_SIZE_BYTES = 64*1024
+timeout = 35
+
+class ResponseReaderInfo(object):
+    def __init__(self):
+        self.did_read_all_data = False
+        self.last_exception = None
+
+
+class FileIntegrityError(Exception):
+    pass
 
 def download_and_unzip(url, zip_file_path, target_folder):
     try:
         response = urlopen(url)
-        with open(zip_file_path, 'wb') as out_file:
-            while True:
-                chunk = response.read(64 * 1024)  # 64 KB chunks
-                if not chunk:
-                    break
-                out_file.write(chunk)
+        print("resp: ", response.getcode())
+        with open(zip_file_path, "wb") as f:
+                while True:
+                    rr_info = ResponseReaderInfo()
+
+                    def read_next_chunk():
+                        try:
+                            chunk = response.read(READ_BUFFER_SIZE_BYTES)
+                            if chunk:
+                                f.write(chunk)
+                            else:
+                                rr_info.did_read_all_data = True
+                        except BaseException as ie:
+                            rr_info.last_exception = ie
+
+                    reader = threading.Thread(target=read_next_chunk)
+                    reader.start()
+                    try:
+                        reader.join(timeout=timeout)
+                    finally:
+                        if reader.is_alive():
+                            print(
+                                "Did not receive the next payload chunk of %r within %.3fs timeout. "
+                                "Assuming the socket read operation is stuck",
+                                url,
+                                timeout,
+                            )
+                            raise FileIntegrityError()
+                    if rr_info.did_read_all_data:
+                        break
+                    if rr_info.last_exception is not None:
+                        raise rr_info.last_exception
+ 
         print("Download completed successfully.")
     except Exception as e:
         print("Error downloading the file: {}".format(e))
